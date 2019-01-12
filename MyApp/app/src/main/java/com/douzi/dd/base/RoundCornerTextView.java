e0b8a8a011e056d0d81223e8e3401585a7ad60c4
zhuxiaohui <douzi@zhuxiaohuideMacBook-Pro.local>
2019-01-12 17:51:34 +0800

    1. 圆角

--- MyApp/app/src/main/java/com/douzi/dd/base/RoundCornerTextView.java
+++ MyApp/app/src/main/java/com/douzi/dd/base/RoundCornerTextView.java
@@ -3,32 +3,29 @@ package com.douzi.dd.base;
 import android.content.Context;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.widget.FrameLayout;
+import android.widget.TextView;
 
-import com.douzi.dd.utils.DeviceUtil;
+import com.douzi.dd.base.RoundCornerViewOutlineProvider;
 
-public class RoundCornerFrameLayout extends FrameLayout {
-    public RoundCornerFrameLayout(Context context) {
+public class RoundCornerTextView extends TextView {
+    public RoundCornerTextView(Context context) {
         super(context);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             setOutlineProvider(new RoundCornerViewOutlineProvider(200));
-            setClipToOutline(true);
         }
     }
 
-    public RoundCornerFrameLayout(Context context,  AttributeSet attrs) {
+    public RoundCornerTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             setOutlineProvider(new RoundCornerViewOutlineProvider(200));
-            setClipToOutline(true);
         }
     }
 
-    public RoundCornerFrameLayout(Context context,  AttributeSet attrs, int defStyleAttr) {
+    public RoundCornerTextView(Context context,  AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             setOutlineProvider(new RoundCornerViewOutlineProvider(200));
-            setClipToOutline(true);
         }
     }
 }
--- MyApp/app/src/main/java/com/douzi/dd/base/RoundCornerViewOutlineProvider.java
+++ MyApp/app/src/main/java/com/douzi/dd/base/RoundCornerViewOutlineProvider.java
@@ -16,12 +16,15 @@ public class RoundCornerViewOutlineProvider extends ViewOutlineProvider {
         this.mRadius = radius;
     }
 
+    int width = 400;
+    int height = 600;
+
     @Override
     public void getOutline(View view, Outline outline) {
         Rect rect = new Rect();
         view.getGlobalVisibleRect(rect);
-        int leftMargin = 0;
-        int topMargin = 0;
+        int leftMargin = (int) (1.0f * (rect.right - rect.left - width) / 2);
+        int topMargin = (int) (1.0f * (rect.bottom - rect.top - height) / 2);
         Rect selfRect = new Rect(leftMargin, topMargin,
                 rect.right - rect.left - leftMargin, rect.bottom - rect.top - topMargin);
         outline.setRoundRect(selfRect, mRadius);
