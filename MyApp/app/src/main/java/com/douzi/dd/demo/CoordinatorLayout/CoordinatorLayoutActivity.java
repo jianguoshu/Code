697b2df99143b2891506ca43312913c0ed70b018
zhuxiaohui <douzi@zhuxiaohuideMacBook-Pro.local>
2019-02-13 12:30:16 +0800

    1. CoordinatorLayout实现WebView滑动隐藏头部(动态切换是否只隐藏频道；动态切换显示、不显示频道)

--- MyApp/app/src/main/java/com/douzi/dd/demo/CoordinatorLayout/CoordinatorLayoutActivity.java
+++ MyApp/app/src/main/java/com/douzi/dd/demo/CoordinatorLayout/CoordinatorLayoutActivity.java
@@ -4,8 +4,11 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.TextView;
 
 import com.douzi.dd.BaseActivity;
 import com.douzi.dd.R;
@@ -13,6 +16,11 @@ import com.douzi.dd.R;
 public class CoordinatorLayoutActivity extends BaseActivity {
 
     private WebView mWebView;
+    private CollapsingToolbarLayout toolbarLayout;
+    private View channelLayout;
+    private int searchBoxHeight;
+    private TextView btnFullScreen;
+    private TextView btnCover;
 
     public static void startAct(Context context) {
         Intent intent = new Intent(context, CoordinatorLayoutActivity.class);
@@ -27,6 +35,8 @@ public class CoordinatorLayoutActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coordinator_layout);
 
+        searchBoxHeight = getResources().getDimensionPixelSize(R.dimen.web_topbar_height);
+
         mWebView = this.findViewById(R.id.wb_content);
         mWebView.setWebViewClient(new WebViewClient() {
             @Override
@@ -37,5 +47,59 @@ public class CoordinatorLayoutActivity extends BaseActivity {
         });
         mWebView.setVerticalScrollBarEnabled(true);
         mWebView.loadUrl("http:wap.baidu.com");
+
+        toolbarLayout = this.findViewById(R.id.toolbar_layout);
+        channelLayout = this.findViewById(R.id.layout_search_result_channel);
+        btnFullScreen = this.findViewById(R.id.btn_full_screen);
+        btnFullScreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchFullScreen();
+            }
+        });
+
+        btnCover = this.findViewById(R.id.btn_cover);
+        btnCover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchCover();
+            }
+        });
+
+        updateFullScreen(isFullScreen);
+        updateCover(isCoverDisplay);
+    }
+
+    boolean isFullScreen = true;
+    boolean isCoverDisplay = false;
+
+    private void switchFullScreen() {
+        isFullScreen = !isFullScreen;
+        updateFullScreen(isFullScreen);
+    }
+
+    private void switchCover() {
+        isCoverDisplay = !isCoverDisplay;
+        updateCover(isCoverDisplay);
+    }
+
+    private void updateFullScreen(boolean isFullScreen) {
+        if (isFullScreen) {
+            toolbarLayout.setMinimumHeight(0);
+            btnFullScreen.setText("关闭全屏");
+        } else {
+            toolbarLayout.setMinimumHeight(searchBoxHeight);
+            btnFullScreen.setText("打开全屏");
+        }
+    }
+
+    private void updateCover(boolean isCoverDisplay) {
+        if (isCoverDisplay) {
+            channelLayout.setVisibility(View.GONE);
+            btnCover.setText("关闭浮层");
+        } else {
+            channelLayout.setVisibility(View.VISIBLE);
+            btnCover.setText("打开浮层");
+        }
     }
 }
